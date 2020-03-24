5bf559845856d30f3c077a022f975107035aed86
erethilclaw <ernest.riccetto@gmail.com>
2020-03-24 10:17:48 +0100

    Preparing contact page

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,22 +3,40 @@
 namespace App\Controller;
 
 use App\Entity\PortofolioPage;
+use App\Form\MailType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * @Route("/", name="front")
      */
-    public function index(EntityManagerInterface $em)
+    public function index()
     {
-        $page = $em->getRepository(PortofolioPage::class)->findOneBy(['slug'=>'portofolioPage']);
+        $portofofiloPage = $this->em->getRepository(PortofolioPage::class)->findOneBy(['slug'=>'portofolioPage']);
 
         return $this->render('front/index.html.twig', [
             'controller_name' => 'FrontController',
-            'portofolioPage' => $page
+            'portofolioPage' => $portofofiloPage
         ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact_page")
+     */
+    public function contact(Request $request)
+    {
+        $form = $this->createForm(MailType::class, $mail);
+        $form->handleRequest($request);
     }
 }
