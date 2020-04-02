d55aca5415eb47cd2015da18efdfa6c423f9c8ae
erethilclaw <ernest.riccetto@gmail.com>
2020-04-02 12:24:59 +0200

    cleaning code in ContactType

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,9 +4,6 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,9 +12,9 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class, ['label' => 'contact_form.name'])
-            ->add('email', EmailType::class, ['label' => 'contact_form.email'])
-            ->add('message', TextareaType::class, ['label' => 'contact_form.message'])
+            ->add('name', null, ['label' => 'contact_form.name'])
+            ->add('email', null, ['label' => 'contact_form.email'])
+            ->add('message', null, ['label' => 'contact_form.message'])
         ;
     }
 
