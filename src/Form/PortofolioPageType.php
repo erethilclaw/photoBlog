58b5d0a9308f2aaef0a1b9669b5d007757635836
erethilclaw <ernest.riccetto@gmail.com>
2020-05-04 17:00:23 +0200

    cleaning css, constraint max for image

--- src/Form/PortofolioPageType.php
+++ src/Form/PortofolioPageType.php
@@ -3,13 +3,11 @@
 namespace App\Form;
 
 use App\Entity\PortofolioPage;
-use App\Transformer\ImageToPageTransformer;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\All;
 use Symfony\Component\Validator\Constraints\Image;
 
 
@@ -17,35 +15,37 @@ class PortofolioPageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $imageConstraints = new All([
+            new Image(['maxSize' => '5M'])
+        ]);
         $builder
             ->add('natureGallery', FileType::class, [
                 'multiple' => true,
                 'label' => 'form.natureGallery',
                 'mapped' => false,
                 'required' => false,
-              /*  'constraints' => [
-                    new Image([
-                        'maxSize' => '5M',
-                    ])
-                ],*/
+                'constraints' => $imageConstraints
             ])
             ->add('eventGallery', FileType::class, [
                 'multiple' => true,
                 'label' => 'form.eventGallery',
                 'mapped' => false,
-                'required' => false
+                'required' => false,
+                'constraints' => $imageConstraints
             ])
             ->add('sesionGallery', FileType::class, [
                 'multiple' => true,
                 'label' => 'form.sesionGallery',
                 'mapped' => false,
-                'required' => false
+                'required' => false,
+                'constraints' => $imageConstraints
             ])
             ->add('showGallery', FileType::class, [
                 'multiple' => true,
                 'label' => 'form.showGallery',
                 'mapped' => false,
-                'required' => false
+                'required' => false,
+                'constraints' => $imageConstraints
             ]);
     }
 
