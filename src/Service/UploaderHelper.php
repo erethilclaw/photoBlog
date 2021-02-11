1510bf2c40cf0d8496da697477d6ee8671069355
erethilclaw <ernest.riccetto@gmail.com>
2021-02-11 12:43:36 +0100

    routing to images tweaks

--- src/Service/UploaderHelper.php
+++ src/Service/UploaderHelper.php
@@ -3,7 +3,7 @@
 
 namespace App\Service;
 
-
+use Symfony\Component\Asset\Context\RequestStackContext;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class UploaderHelper
@@ -11,22 +11,24 @@ class UploaderHelper
     const ARTICLE_IMAGE = 'article_image';
 
     private $uploadPath;
+    private $requestStackContext;
 
-    public function __construct(string $uploadPath )
+    public function __construct(string $uploadPath, RequestStackContext $requestStackContext)
     {
         $this->uploadPath = $uploadPath;
+        $this->requestStackContext = $requestStackContext;
     }
 
     public function getPublicPath(string $path): string
     {
-        return 'uploads/'.$path;
+        return $this->requestStackContext->getBasePath() . '/uploads/' . $path;
     }
 
     public function uploadArticleImage(UploadedFile $uploadedFile): string
     {
-        $detination = $this->uploadPath.'/'.self::ARTICLE_IMAGE;
+        $detination = $this->uploadPath . '/' . self::ARTICLE_IMAGE;
         $originalFileName = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
-        $newFileName = $originalFileName.'-'.uniqid().'.'.$uploadedFile->guessExtension();
+        $newFileName = $originalFileName . '-' . uniqid() . '.' . $uploadedFile->guessExtension();
         $uploadedFile->move($detination, $newFileName);
 
         return $newFileName;
