92544c68fa37afa99f254229daa6182e7e85640a
erethilclaw <ernest.riccetto@gmail.com>
2021-05-13 13:02:06 +0200

    beta overlay hamburger

--- public/js/general.js
+++ public/js/general.js
@@ -2,10 +2,14 @@
 //hamburger
 $(function() {
     $(".hamburgerBtn").on("click", function() {
+        document.getElementById("overlay").style.display = "block";
         if ($(".nav-links ").hasClass("active")) {
             $(".nav-links ").removeClass("active");
         } else {
             $(".nav-links ").addClass("active");
         }
     });
+    $("#overlay").on("click", function() {
+        document.getElementById("overlay").style.display = "none";
+    })   
 });
